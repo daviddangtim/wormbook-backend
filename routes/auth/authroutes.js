f39dfe295a6ee1ffb1391f7420e84c28d426d838
daviddangtim <132236718+daviddangtim@users.noreply.github.com>
2024-05-10 14:25:48 +0100

    Revert "Worm Book"
    This reverts commit 085b7e74e7e6cea233404f4b8eb442458a086d1e.

--- routes/auth/authroutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const Router = express.Router()
-import { signUp, signIn } from "../../controllers/authcontroller.js";
-
-Router.post("/register",signUp)
-Router.post("/login", signIn)
-export default Router;
